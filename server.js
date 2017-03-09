83c8bfb1873ce076a0aa5246249740d6fdc91934
Alejandro <alejandrochvs@hotmail.com>
2017-03-09 17:05:58 +0000

    -

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongo.MongoClient.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017
     size: 5242880,
     max: 5000
   });
-  //routes(app, db);
+  routes(app, db);
   //api(app, db);
 });
 
