b71c941947fd7244f2c7e6fcaff48438cc6cb263
Alejandro <alejandrochvs@hotmail.com>
2017-03-09 16:12:45 +0000

    Changed to pug

--- public/script/main.js
+++ public/script/main.js
@@ -1,1 +1,17 @@
-console.log("Js true");
+$(function() {
+    var localHref = window.location.href;
+    var firstHref = $('.first').attr('href');
+    var secondHref = $('.second').attr('href');
+    
+    $('.first').attr('href', localHref + "new/" + firstHref);
+    $('.second').attr('href', localHref + "new/" + secondHref);
+    
+    $('#Go').on('click',function(){
+        var info = $('.input').val();
+        if (info == "")
+        {
+            info = "https://www.google.com/";
+        }
+        window.location.href = localHref + "new/" + info;
+    });
+});
--- server.js
+++ server.js
@@ -1,23 +1,33 @@
-var express    = require('express');
-var app        = express();
+var express = require('express');
 var path = require('path');
+var mongo = require('mongodb');
+var routes = require('./public/routes/index.js');
+var api = require('./public/api/url-shortener.js');
+var app = express();
 var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/public'));
+
+require('dotenv').config({
+  silent: true
+});
+
+mongo.MongoClient.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/url-shortener', function(err, db) {
+  if (err) {
+    throw new Error('Database failed to connect!');
+  } else {
+    console.log('Successfully connected to MongoDB on port 27017.');
+  }
+  db.createCollection("sites", {
+    capped: true,
+    size: 5242880,
+    max: 5000
+  });
+  routes(app, db);
+  api(app, db);
+});
 
 app.listen(port, function() {
     console.log('App listening on port ' + port);
 });
-
-app.get('/', function(req, res) {
-  var fileName = path.join(__dirname, 'index.html');
-  res.sendFile(fileName, function (err) {
-    if (err) {
-      console.log(err);
-      res.status(err.status).end();
-    }
-    else {
-      console.log('Server update:', fileName);
-    }
-  });
-});
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'pug');;
